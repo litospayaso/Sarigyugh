94049384e7274be721d2a8a19f96e0e3064e9854
Hita <697729@cognizant.com>
2018-11-22 16:27:51 +0100

    removing check files uploaded

--- upload.js
+++ upload.js
@@ -9,64 +9,58 @@ let assetsFiles = 0;
 const connection = require(`${__dirname}\\connection.json`);
 
 const sendFile = async () => {
-    await fs.readdirSync(`${__dirname}\\dist\\salome`).forEach(async file => {
-        rootFiles += 1;
-        if (file !== 'assets'){
-            await sftp.fastPut(`${__dirname}\\dist\\salome\\${file}`,`/salome/${file}`).catch((err) => {
-                console.log(err, 'catch error');
-            });
-        }
-    });
-    await fs.readdirSync(`${__dirname}\\dist\\salome\\assets`).forEach(async file => {
-        assetsFiles += 1;
-        if (file !== 'portfolio.pdf'){
-            await sftp.fastPut(`${__dirname}\\dist\\salome\\assets\\${file}`,`/salome/assets/${file}`).catch((err) => {
-                console.log(err, 'catch error');
-            });
-        }
-    });
-    return true;
+  await fs.readdirSync(`${__dirname}\\dist\\salome`).forEach(async file => {
+    rootFiles += 1;
+    if (file !== 'assets') {
+      await sftp.fastPut(`${__dirname}\\dist\\salome\\${file}`, `/salome/${file}`).catch((err) => {
+        console.log(err, 'catch error');
+      });
+    }
+  });
+  await fs.readdirSync(`${__dirname}\\dist\\salome\\assets`).forEach(async file => {
+    assetsFiles += 1;
+    if (file !== 'portfolio.pdf') {
+      await sftp.fastPut(`${__dirname}\\dist\\salome\\assets\\${file}`, `/salome/assets/${file}`).catch((err) => {
+        console.log(err, 'catch error');
+      });
+    }
+  });
+  return true;
 };
 
-const deleteData = async (data,path) => {
-    console.log(data);
-    await data.forEach(async file => {
-        rootFiles += 1;
-        if (file.type !== 'd' && file.name !== 'portfolio.pdf'){
-            await sftp.delete(`${path}/${file.name}`).catch((err) => {
-                console.log(err, 'catch error');
-            });
-        }
-    });
-    return true;
+const deleteData = async (data, path) => {
+  await data.forEach(async file => {
+    rootFiles += 1;
+    if (file.type !== 'd' && file.name !== 'portfolio.pdf') {
+      await sftp.delete(`${path}/${file.name}`).catch((err) => {
+        console.log(err, 'catch error');
+      });
+    }
+  });
+  return true;
 };
 
 sftp.connect(connection).then(() => {
-    return sftp.list('/salome');
+  return sftp.list('/salome');
 }).then(async (data) => {
-    await deleteData(data,'/salome');
-    return true;
+  await deleteData(data, '/salome');
+  return true;
 }).then(() => {
-    return sftp.list('/salome/assets');
+  return sftp.list('/salome/assets');
 }).then(async (data) => {
-    await deleteData(data,'/salome/assets');
-    return true;
-}).then(async() => {
-    await sendFile();
-    return true;
+  await deleteData(data, '/salome/assets');
+  return true;
+}).then(async () => {
+  await sendFile();
+  return true;
 }).then(() => {
-    return sftp.list('/salome');
+  return sftp.list('/salome');
 }).then((data) => {
-    sftp.end();
-    if (data.length === rootFiles){
-        console.log('\x1b[32m%s\x1b[37m%s\x1b[0m','SUCCESS: ', `${rootFiles-1} Updated; ${assetsFiles} asset files`);
-        opn('http://salomevilas.me');
-    }else{
-        console.log(data);
-        console.log('\x1b[31m%s\x1b[37m%s\x1b[0m','ERROR: ','No all data were uploaded');
-    }
-    return true;
+  sftp.end();
+  console.log('\x1b[32m%s\x1b[37m%s\x1b[0m', 'SUCCESS: ', `${rootFiles-1} Updated; ${assetsFiles} asset files`);
+  opn('http://salomevilas.me');
+  return true;
 }).catch((err) => {
-    console.log(err, 'catch error');
-    sftp.end();
+  console.log(err, 'catch error');
+  sftp.end();
 });
