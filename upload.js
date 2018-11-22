86cb16897725eb93f248e04baa0f082cb1ef33d6
Hita <697729@cognizant.com>
2018-11-22 16:26:11 +0100

    updating upload without delete portfolio

--- upload.js
+++ upload.js
@@ -28,12 +28,12 @@ const sendFile = async () => {
     return true;
 };
 
-const deleteData = async (data) => {
+const deleteData = async (data,path) => {
     console.log(data);
     await data.forEach(async file => {
         rootFiles += 1;
-        if (file !== 'assets'){
-            await sftp.fastPut(`${__dirname}\\dist\\salome\\${file}`,`/salome/${file}`).catch((err) => {
+        if (file.type !== 'd' && file.name !== 'portfolio.pdf'){
+            await sftp.delete(`${path}/${file.name}`).catch((err) => {
                 console.log(err, 'catch error');
             });
         }
@@ -44,27 +44,28 @@ const deleteData = async (data) => {
 sftp.connect(connection).then(() => {
     return sftp.list('/salome');
 }).then(async (data) => {
-    await deleteData(data);
-//     return sftp.rmdir('/salome',true);
-// }).then(() => {
-//     return sftp.mkdir('/salome');
-// }).then(() => {
-//     return sftp.mkdir('/salome/assets');
-// }).then(async() => {
-//     await sendFile();
-//     return true;
-// }).then(() => {
-//     return sftp.list('/salome');
-// }).then((data) => {
-//     sftp.end();
-//     if (data.length === rootFiles){
-//         console.log('\x1b[32m%s\x1b[37m%s\x1b[0m','SUCCESS: ', `${rootFiles-1} Updated; ${assetsFiles} asset files`);
-//         opn('http://salomevilas.me');
-//     }else{
-//         console.log(data);
-//         console.log('\x1b[31m%s\x1b[37m%s\x1b[0m','ERROR: ','No all data were uploaded');
-//     }
-//     return true;
+    await deleteData(data,'/salome');
+    return true;
+}).then(() => {
+    return sftp.list('/salome/assets');
+}).then(async (data) => {
+    await deleteData(data,'/salome/assets');
+    return true;
+}).then(async() => {
+    await sendFile();
+    return true;
+}).then(() => {
+    return sftp.list('/salome');
+}).then((data) => {
+    sftp.end();
+    if (data.length === rootFiles){
+        console.log('\x1b[32m%s\x1b[37m%s\x1b[0m','SUCCESS: ', `${rootFiles-1} Updated; ${assetsFiles} asset files`);
+        opn('http://salomevilas.me');
+    }else{
+        console.log(data);
+        console.log('\x1b[31m%s\x1b[37m%s\x1b[0m','ERROR: ','No all data were uploaded');
+    }
+    return true;
 }).catch((err) => {
     console.log(err, 'catch error');
     sftp.end();
